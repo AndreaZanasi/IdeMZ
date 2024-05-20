367166437bdb21b96f849f8e976172a5718bb2b5
AndreaZanasi <zanasi.ndr@gmail.com>
2024-05-20 03:59:49 +0200

    refactor

--- src/main/java/com/IdeMZ/IdeMZApplication.java
+++ src/main/java/com/IdeMZ/IdeMZApplication.java
@@ -35,6 +35,7 @@ public class IdeMZApplication extends Application {
     private Button saveFileButton;
     private Button runButton;
     private String currentDialect = "default_dialect";
+    SyntaxHighlighter syntaxHighlighter = new SyntaxHighlighter("default_dialect");
 
 
     @Override
@@ -43,12 +44,47 @@ public class IdeMZApplication extends Application {
         Image applicationIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/logo.png")));
         primaryStage.getIcons().add(applicationIcon);
 
-        FileOpener fileOpener = new FileOpener(primaryStage);
-        SyntaxHighlighter syntaxHighlighter = new SyntaxHighlighter("default_dialect");
-
         textArea.textProperty().addListener((obs, oldText, newText) -> syntaxHighlighter.highlight(textArea));
 
+        //buttons
         openFileButton = createButton();
+        configureOpenFileButton(primaryStage);
+
+        saveFileButton = new Button();
+        configureSaveFileButton(primaryStage);
+
+        runButton = new Button();
+        configureRunButton();
+
+        translateButton = new MenuButton();
+        configureTranslateButton();
+
+        settingsButton = createMenuButton();
+        configureSettingsButton();
+
+        //hbox for buttons
+        hbox = new HBox(openFileButton, settingsButton, saveFileButton, runButton, translateButton);
+        hbox.setSpacing(20);
+        hbox.setFillHeight(true);
+
+        setDarkModeStyle();
+
+        VBox vbox = new VBox(hbox, textArea);
+        vbox.setFillWidth(true);
+        VBox.setVgrow(textArea, Priority.ALWAYS);
+
+        textArea.getStyleClass().add("text-area-big-font");
+
+        Scene scene = new Scene(vbox, 800, 600);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles.css")).toExternalForm());
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void configureOpenFileButton(Stage primaryStage) {
+        openFileButton.setPrefSize(20, 20);
+        FileOpener fileOpener = new FileOpener(primaryStage);
         openFileButton.setOnAction(event -> {
             File file = fileOpener.openFile();
             if (file != null) {
@@ -63,8 +99,9 @@ public class IdeMZApplication extends Application {
                 }
             }
         });
+    }
 
-        saveFileButton = new Button();
+    private void configureSaveFileButton(Stage primaryStage) {
         saveFileButton.setPrefSize(20, 20);
         saveFileButton.setOnAction(event -> {
             if (currentFile == null || !currentFile.exists()) {
@@ -72,31 +109,24 @@ public class IdeMZApplication extends Application {
                 fileChooser.setTitle("Save File");
                 currentFile = fileChooser.showSaveDialog(primaryStage);
                 if (currentFile == null) {
-                    // User didn't choose a file, return
                     return;
                 }
             }
             try {
                 Files.writeString(currentFile.toPath(), textArea.getText(), StandardOpenOption.CREATE);
                 String command = String.format("java -jar src/main/resources/CompilerMZ-1.0.0-Stable-jar-with-dependencies.jar -i %s --format", currentFile.getAbsolutePath());
-                ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", command);
-                Process process = processBuilder.start();
-                int exitCode = process.waitFor();
-                if (exitCode != 0) {
-                    LOGGER.log(Level.SEVERE, "The process exited with error code: " + exitCode);
-                    return;
-                }
+                executeCommand(command);
                 // Reload the text area with the updated file
                 String content = Files.readString(currentFile.toPath());
                 textArea.replaceText(content);
                 syntaxHighlighter.highlight(textArea);
             } catch (IOException e) {
                 LOGGER.log(Level.SEVERE, "An IO exception occurred", e);
-            } catch (InterruptedException e) {
-                LOGGER.log(Level.SEVERE, "An InterruptedException occurred", e);
             }
         });
+    }
 
+    private void configureRunButton() {
         runButton = new Button();
         runButton.setPrefSize(20, 20);
         runButton.setOnAction(event -> {
@@ -110,15 +140,21 @@ public class IdeMZApplication extends Application {
                 try {
                     String filePath = currentFile.getAbsolutePath();
                     String filePathWithoutExtension = filePath.substring(0, filePath.lastIndexOf('.'));
-                    String command = String.format("java -jar src/main/resources/CompilerMZ-1.0.0-Stable-jar-with-dependencies.jar -i %s && %s; exec bash", filePath, filePathWithoutExtension);
+                    String command;
+                    if (!currentDialect.equals("default_dialect")) {
+                        command = String.format("java -jar src/main/resources/CompilerMZ-1.0.0-Stable-jar-with-dependencies.jar -i %s -d %s && %s; exec bash", filePath, currentDialect, filePathWithoutExtension);
+                    } else {
+                        command = String.format("java -jar src/main/resources/CompilerMZ-1.0.0-Stable-jar-with-dependencies.jar -i %s && %s; exec bash", filePath, filePathWithoutExtension);
+                    }
                     Runtime.getRuntime().exec(new String[]{"gnome-terminal", "--", "bash", "-c", command});
                 } catch (IOException e) {
                     LOGGER.log(Level.SEVERE, "An IO exception occurred", e);
                 }
             }
         });
+    }
 
-        translateButton = new MenuButton("Translate");
+    private void configureTranslateButton() {
         translateButton.setPrefSize(20, 20);
         File dialectsDir = new File(Objects.requireNonNull(getClass().getResource("/dialects")).getFile());
         File[] dialectFiles = dialectsDir.listFiles((dir, name) -> name.endsWith(".json"));
@@ -138,13 +174,7 @@ public class IdeMZApplication extends Application {
                         }
                         String currentFilePath = currentFile.getAbsolutePath();
                         String command = String.format("java -jar src/main/resources/CompilerMZ-1.0.0-Stable-jar-with-dependencies.jar -i %s -t %s,%s", currentFilePath, currentDialect, dialectName);
-                        ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", command);
-                        Process process = processBuilder.start();
-                        int exitCode = process.waitFor();
-                        if (exitCode != 0) {
-                            LOGGER.log(Level.SEVERE, "The process exited with error code: " + exitCode);
-                            return;
-                        }
+                        executeCommand(command);
 
                         // Reload the text area with the updated file
                         String content = Files.readString(currentFile.toPath());
@@ -156,40 +186,21 @@ public class IdeMZApplication extends Application {
 
                         // Update the current dialect
                         currentDialect = dialectName;
-                    } catch (IOException | InterruptedException e) {
+                    } catch (IOException e) {
                         LOGGER.log(Level.SEVERE, "An exception occurred", e);
                     }
                 });
                 translateButton.getItems().add(menuItem);
             }
         }
+    }
 
-        settingsButton = createMenuButton();
+    private void configureSettingsButton() {
+        settingsButton.setPrefSize(20, 20);
         Dialog<Void> styleDialog = createStyleDialog();
         MenuItem styleButton = new MenuItem("Style");
         settingsButton.getItems().add(styleButton);
         styleButton.setOnAction(event -> styleDialog.show());
-
-        hbox = new HBox(openFileButton, settingsButton, saveFileButton, runButton, translateButton);
-        hbox.setSpacing(20);
-        hbox.setFillHeight(true);
-
-        setDarkModeStyle();
-
-        VBox vbox = new VBox(hbox, textArea);
-        vbox.setFillWidth(true);
-        VBox.setVgrow(textArea, Priority.ALWAYS);
-
-        openFileButton.setPrefSize(20, 20);
-        settingsButton.setPrefSize(20, 20);
-
-        textArea.getStyleClass().add("text-area-big-font");
-
-        Scene scene = new Scene(vbox, 800, 600);
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles.css")).toExternalForm());
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
     private Button createButton() {
@@ -255,47 +266,12 @@ public class IdeMZApplication extends Application {
         String darkModeColor = "-fx-background-color: #31363F;";
         textArea.setStyle("-fx-background-color: #222831; -fx-text-fill: #EEEEEE;");
         hbox.setStyle(darkModeColor);
-        openFileButton.setStyle(darkModeColor);
-        settingsButton.setStyle(darkModeColor);
-        saveFileButton.setStyle(darkModeColor);
-        runButton.setStyle(darkModeColor);
-        translateButton.setStyle(darkModeColor);
 
-        double imageWidth = 30.0;
-        double imageHeight = 30.0;
-
-        Image file_white = new Image(Objects.requireNonNull(getClass().getResource("/images/file_white.png")).toExternalForm());
-        ImageView file_white_view = new ImageView(file_white);
-        file_white_view.setFitWidth(imageWidth);
-        file_white_view.setFitHeight(imageHeight);
-
-        Image gear_white = new Image(Objects.requireNonNull(getClass().getResource("/images/gear_white.png")).toExternalForm());
-        ImageView gear_white_view = new ImageView(gear_white);
-        gear_white_view.setFitWidth(imageWidth);
-        gear_white_view.setFitHeight(imageHeight);
-
-        // Inside the setDarkModeStyle method, after setting the graphic for the openFileButton
-        Image save_white = new Image(Objects.requireNonNull(getClass().getResource("/images/save_white.png")).toExternalForm());
-        ImageView save_white_view = new ImageView(save_white);
-        save_white_view.setFitWidth(30.0);
-        save_white_view.setFitHeight(30.0);
-
-        // Inside the setDarkModeStyle method, after setting the graphic for the saveFileButton
-        Image play_white = new Image(Objects.requireNonNull(getClass().getResource("/images/play_white.png")).toExternalForm());
-        ImageView play_white_view = new ImageView(play_white);
-        play_white_view.setFitWidth(30.0);
-        play_white_view.setFitHeight(30.0);
-
-        Image world_white = new Image(Objects.requireNonNull(getClass().getResource("/images/world_white.png")).toExternalForm());
-        ImageView world_white_view = new ImageView(world_white);
-        world_white_view.setFitWidth(30.0);
-        world_white_view.setFitHeight(30.0);
-
-        openFileButton.setGraphic(file_white_view);
-        settingsButton.setGraphic(gear_white_view);
-        saveFileButton.setGraphic(save_white_view);
-        runButton.setGraphic(play_white_view);
-        translateButton.setGraphic(world_white_view);
+        setButtonStyleAndGraphic(openFileButton, darkModeColor, new Image(Objects.requireNonNull(getClass().getResource("/images/file_white.png")).toExternalForm()));
+        setButtonStyleAndGraphic(settingsButton, darkModeColor, new Image(Objects.requireNonNull(getClass().getResource("/images/gear_white.png")).toExternalForm()));
+        setButtonStyleAndGraphic(saveFileButton, darkModeColor, new Image(Objects.requireNonNull(getClass().getResource("/images/save_white.png")).toExternalForm()));
+        setButtonStyleAndGraphic(runButton, darkModeColor, new Image(Objects.requireNonNull(getClass().getResource("/images/play_white.png")).toExternalForm()));
+        setButtonStyleAndGraphic(translateButton, darkModeColor, new Image(Objects.requireNonNull(getClass().getResource("/images/world_white.png")).toExternalForm()));
 
         textArea.getStyleClass().add("dark");
         isDarkMode = true;
@@ -308,52 +284,38 @@ public class IdeMZApplication extends Application {
         String lightModeColor = "-fx-background-color: #9394A5;";
         textArea.setStyle("-fx-background-color: #fafafa; -fx-text-fill: #000000;");
         hbox.setStyle(lightModeColor);
-        openFileButton.setStyle(lightModeColor);
-        settingsButton.setStyle(lightModeColor);
-        saveFileButton.setStyle(lightModeColor);
-        runButton.setStyle(lightModeColor);
-        translateButton.setStyle(lightModeColor);
 
-        double imageWidth = 30.0;
-        double imageHeight = 30.0;
-
-        Image file_black = new Image(Objects.requireNonNull(getClass().getResource("/images/file_black.png")).toExternalForm());
-        ImageView file_black_view = new ImageView(file_black);
-        file_black_view.setFitWidth(imageWidth);
-        file_black_view.setFitHeight(imageHeight);
-
-        Image gear_black = new Image(Objects.requireNonNull(getClass().getResource("/images/gear_black.png")).toExternalForm());
-        ImageView gear_black_view = new ImageView(gear_black);
-        gear_black_view.setFitWidth(imageWidth);
-        gear_black_view.setFitHeight(imageHeight);
-
-        // Inside the setLightModeStyle method, after setting the graphic for the openFileButton
-        Image save_black = new Image(Objects.requireNonNull(getClass().getResource("/images/save_black.png")).toExternalForm());
-        ImageView save_black_view = new ImageView(save_black);
-        save_black_view.setFitWidth(30.0);
-        save_black_view.setFitHeight(30.0);
-
-        // Inside the setLightModeStyle method, after setting the graphic for the saveFileButton
-        Image play_black = new Image(Objects.requireNonNull(getClass().getResource("/images/play_black.png")).toExternalForm());
-        ImageView play_black_view = new ImageView(play_black);
-        play_black_view.setFitWidth(30.0);
-        play_black_view.setFitHeight(30.0);
-
-        Image world_black = new Image(Objects.requireNonNull(getClass().getResource("/images/world_black.png")).toExternalForm());
-        ImageView world_black_view = new ImageView(world_black);
-        world_black_view.setFitWidth(30.0);
-        world_black_view.setFitHeight(30.0);
-
-        openFileButton.setGraphic(file_black_view);
-        settingsButton.setGraphic(gear_black_view);
-        saveFileButton.setGraphic(save_black_view);
-        runButton.setGraphic(play_black_view);
-        translateButton.setGraphic(world_black_view);
+        setButtonStyleAndGraphic(openFileButton, lightModeColor, new Image(Objects.requireNonNull(getClass().getResource("/images/file_black.png")).toExternalForm()));
+        setButtonStyleAndGraphic(settingsButton, lightModeColor, new Image(Objects.requireNonNull(getClass().getResource("/images/gear_black.png")).toExternalForm()));
+        setButtonStyleAndGraphic(saveFileButton, lightModeColor, new Image(Objects.requireNonNull(getClass().getResource("/images/save_black.png")).toExternalForm()));
+        setButtonStyleAndGraphic(runButton, lightModeColor, new Image(Objects.requireNonNull(getClass().getResource("/images/play_black.png")).toExternalForm()));
+        setButtonStyleAndGraphic(translateButton, lightModeColor, new Image(Objects.requireNonNull(getClass().getResource("/images/world_black.png")).toExternalForm()));
 
         textArea.getStyleClass().remove("dark");
         textArea.setStyle("-fx-fill: black;");
 
         isDarkMode = false;
+    }
+
+    private void setButtonStyleAndGraphic(ButtonBase button, String style, Image image) {
+        button.setStyle(style);
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(30.0);
+        imageView.setFitHeight(30.0);
+        button.setGraphic(imageView);
+    }
+
+    private void executeCommand(String command) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", command);
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                LOGGER.log(Level.SEVERE, "The process exited with error code: " + exitCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            LOGGER.log(Level.SEVERE, "An exception occurred", e);
+        }
     }
 
     public static void main(String[] args) {
