a41d1685260128de067e5d3e7fea7a67322ac7c6
AndreaZanasi <zanasi.ndr@gmail.com>
2024-05-20 03:28:38 +0200

    Added translation logic

--- src/main/java/com/IdeMZ/IdeMZApplication.java
+++ src/main/java/com/IdeMZ/IdeMZApplication.java
@@ -3,15 +3,13 @@ package com.IdeMZ;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.fxmisc.richtext.StyleClassedTextArea;
@@ -32,21 +30,25 @@ public class IdeMZApplication extends Application {
     private HBox hbox;
     private Button openFileButton;
     private MenuButton settingsButton;
+    private MenuButton translateButton;
     private File currentFile;
     private Button saveFileButton;
     private Button runButton;
+    private String currentDialect = "default_dialect";
 
 
     @Override
     public void start(Stage primaryStage) {
+
+        Image applicationIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/logo.png")));
+        primaryStage.getIcons().add(applicationIcon);
+
         FileOpener fileOpener = new FileOpener(primaryStage);
-        SyntaxHighlighter syntaxHighlighter = new SyntaxHighlighter();
+        SyntaxHighlighter syntaxHighlighter = new SyntaxHighlighter("default_dialect");
 
         textArea.textProperty().addListener((obs, oldText, newText) -> syntaxHighlighter.highlight(textArea));
 
         openFileButton = createButton();
-        settingsButton = createMenuButton();
-
         openFileButton.setOnAction(event -> {
             File file = fileOpener.openFile();
             if (file != null) {
@@ -65,34 +67,46 @@ public class IdeMZApplication extends Application {
         saveFileButton = new Button();
         saveFileButton.setPrefSize(20, 20);
         saveFileButton.setOnAction(event -> {
-            if (currentFile != null) {
-                try {
-                    Files.writeString(currentFile.toPath(), textArea.getText(), StandardOpenOption.TRUNCATE_EXISTING);
-                    // TODO Only format the file in default dialect
-                    String command = String.format("java -jar src/main/resources/CompilerMZ-1.0.0-Stable-jar-with-dependencies.jar -i %s --format", currentFile.getAbsolutePath());
-                    ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", command);
-                    Process process = processBuilder.start();
-                    int exitCode = process.waitFor();
-                    if (exitCode != 0) {
-                        LOGGER.log(Level.SEVERE, "The process exited with error code: " + exitCode);
-                        return;
-                    }
-                    // Reload the text area with the updated file
-                    String content = Files.readString(currentFile.toPath());
-                    textArea.replaceText(content);
-                    syntaxHighlighter.highlight(textArea);
-                } catch (IOException e) {
-                    LOGGER.log(Level.SEVERE, "An IO exception occurred", e);
-                } catch (InterruptedException e) {
-                    LOGGER.log(Level.SEVERE, "An InterruptedException occurred", e);
+            if (currentFile == null || !currentFile.exists()) {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Save File");
+                currentFile = fileChooser.showSaveDialog(primaryStage);
+                if (currentFile == null) {
+                    // User didn't choose a file, return
+                    return;
                 }
+            }
+            try {
+                Files.writeString(currentFile.toPath(), textArea.getText(), StandardOpenOption.CREATE);
+                String command = String.format("java -jar src/main/resources/CompilerMZ-1.0.0-Stable-jar-with-dependencies.jar -i %s --format", currentFile.getAbsolutePath());
+                ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", command);
+                Process process = processBuilder.start();
+                int exitCode = process.waitFor();
+                if (exitCode != 0) {
+                    LOGGER.log(Level.SEVERE, "The process exited with error code: " + exitCode);
+                    return;
+                }
+                // Reload the text area with the updated file
+                String content = Files.readString(currentFile.toPath());
+                textArea.replaceText(content);
+                syntaxHighlighter.highlight(textArea);
+            } catch (IOException e) {
+                LOGGER.log(Level.SEVERE, "An IO exception occurred", e);
+            } catch (InterruptedException e) {
+                LOGGER.log(Level.SEVERE, "An InterruptedException occurred", e);
             }
         });
 
         runButton = new Button();
         runButton.setPrefSize(20, 20);
         runButton.setOnAction(event -> {
-            if (currentFile != null) {
+            if (currentFile == null || !currentFile.exists()) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Warning Dialog");
+                alert.setHeaderText(null);
+                alert.setContentText("Before running the file please save it.");
+                alert.showAndWait();
+            } else {
                 try {
                     String filePath = currentFile.getAbsolutePath();
                     String filePathWithoutExtension = filePath.substring(0, filePath.lastIndexOf('.'));
@@ -104,13 +118,60 @@ public class IdeMZApplication extends Application {
             }
         });
 
+        translateButton = new MenuButton("Translate");
+        translateButton.setPrefSize(20, 20);
+        File dialectsDir = new File(Objects.requireNonNull(getClass().getResource("/dialects")).getFile());
+        File[] dialectFiles = dialectsDir.listFiles((dir, name) -> name.endsWith(".json"));
+        if (dialectFiles != null) {
+            for (File dialectFile : dialectFiles) {
+                String dialectName = dialectFile.getName().replace(".json", "");
+                MenuItem menuItem = new MenuItem(dialectName);
+                menuItem.setOnAction(event -> {
+                    try {
+                        if (currentFile == null || !currentFile.exists()) {
+                            Alert alert = new Alert(Alert.AlertType.WARNING);
+                            alert.setTitle("Warning Dialog");
+                            alert.setHeaderText(null);
+                            alert.setContentText("Before translate the file, please save the file.");
+                            alert.showAndWait();
+                            return;
+                        }
+                        String currentFilePath = currentFile.getAbsolutePath();
+                        String command = String.format("java -jar src/main/resources/CompilerMZ-1.0.0-Stable-jar-with-dependencies.jar -i %s -t %s,%s", currentFilePath, currentDialect, dialectName);
+                        ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", command);
+                        Process process = processBuilder.start();
+                        int exitCode = process.waitFor();
+                        if (exitCode != 0) {
+                            LOGGER.log(Level.SEVERE, "The process exited with error code: " + exitCode);
+                            return;
+                        }
 
+                        // Reload the text area with the updated file
+                        String content = Files.readString(currentFile.toPath());
+                        textArea.replaceText(content);
+
+                        // Update the SyntaxHighlighter with the new dialect
+                        syntaxHighlighter.updateDialect(dialectName);
+                        syntaxHighlighter.highlight(textArea);
+
+                        // Update the current dialect
+                        currentDialect = dialectName;
+                    } catch (IOException | InterruptedException e) {
+                        LOGGER.log(Level.SEVERE, "An exception occurred", e);
+                    }
+                });
+                translateButton.getItems().add(menuItem);
+            }
+        }
+
+        settingsButton = createMenuButton();
         Dialog<Void> styleDialog = createStyleDialog();
         MenuItem styleButton = new MenuItem("Style");
         settingsButton.getItems().add(styleButton);
         styleButton.setOnAction(event -> styleDialog.show());
 
-        hbox = new HBox(openFileButton, settingsButton, saveFileButton, runButton);
+        hbox = new HBox(openFileButton, settingsButton, saveFileButton, runButton, translateButton);
+        hbox.setSpacing(20);
         hbox.setFillHeight(true);
 
         setDarkModeStyle();
@@ -191,15 +252,14 @@ public class IdeMZApplication extends Application {
         if (isDarkMode) {
             return;
         }
-
         String darkModeColor = "-fx-background-color: #31363F;";
-
         textArea.setStyle("-fx-background-color: #222831; -fx-text-fill: #EEEEEE;");
         hbox.setStyle(darkModeColor);
         openFileButton.setStyle(darkModeColor);
         settingsButton.setStyle(darkModeColor);
         saveFileButton.setStyle(darkModeColor);
         runButton.setStyle(darkModeColor);
+        translateButton.setStyle(darkModeColor);
 
         double imageWidth = 30.0;
         double imageHeight = 30.0;
@@ -226,11 +286,16 @@ public class IdeMZApplication extends Application {
         play_white_view.setFitWidth(30.0);
         play_white_view.setFitHeight(30.0);
 
+        Image world_white = new Image(Objects.requireNonNull(getClass().getResource("/images/world_white.png")).toExternalForm());
+        ImageView world_white_view = new ImageView(world_white);
+        world_white_view.setFitWidth(30.0);
+        world_white_view.setFitHeight(30.0);
 
         openFileButton.setGraphic(file_white_view);
         settingsButton.setGraphic(gear_white_view);
         saveFileButton.setGraphic(save_white_view);
         runButton.setGraphic(play_white_view);
+        translateButton.setGraphic(world_white_view);
 
         textArea.getStyleClass().add("dark");
         isDarkMode = true;
@@ -240,15 +305,14 @@ public class IdeMZApplication extends Application {
         if (!isDarkMode) {
             return;
         }
-
         String lightModeColor = "-fx-background-color: #9394A5;";
-
         textArea.setStyle("-fx-background-color: #fafafa; -fx-text-fill: #000000;");
         hbox.setStyle(lightModeColor);
         openFileButton.setStyle(lightModeColor);
         settingsButton.setStyle(lightModeColor);
         saveFileButton.setStyle(lightModeColor);
         runButton.setStyle(lightModeColor);
+        translateButton.setStyle(lightModeColor);
 
         double imageWidth = 30.0;
         double imageHeight = 30.0;
@@ -275,11 +339,16 @@ public class IdeMZApplication extends Application {
         play_black_view.setFitWidth(30.0);
         play_black_view.setFitHeight(30.0);
 
+        Image world_black = new Image(Objects.requireNonNull(getClass().getResource("/images/world_black.png")).toExternalForm());
+        ImageView world_black_view = new ImageView(world_black);
+        world_black_view.setFitWidth(30.0);
+        world_black_view.setFitHeight(30.0);
 
         openFileButton.setGraphic(file_black_view);
         settingsButton.setGraphic(gear_black_view);
         saveFileButton.setGraphic(save_black_view);
         runButton.setGraphic(play_black_view);
+        translateButton.setGraphic(world_black_view);
 
         textArea.getStyleClass().remove("dark");
         textArea.setStyle("-fx-fill: black;");
